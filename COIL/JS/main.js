fcf98d9d5a2c4a30395239c942136c66082890f6
El-Shonkee <108562297+El-Shonkee@users.noreply.github.com>
2023-03-04 17:09:36 +0000

    se agrego canvas y section del mapa

--- COIL/JS/main.js
+++ COIL/JS/main.js
@@ -1,14 +1,23 @@
+const showMap = document.getElementById('showMap');
+const mapcanvas = document.getElementById('mapcanvas');
 
+let ctx = mapcanvas.getContext('2d');
+
+class Player{
+    constructor(name, photo, points){
+        this.name = name;
+        this.photo = photo ;
+        this.points = points;
+    } 
+}
+
+let warrior = new Player("Warrior", "/workspaces/COIL/COIL/images/warrior.png)", 0);
 
 
 function startGame(){
-    const showMap = document.getElementById('showMap');
-    const canvas =document.getElementById('canvas');
     showMap.style.display = "none"
-
     let buttonLetsStart = document.getElementById("character-button");
-    buttonLetsStart.addEventListener('click',selectCharacter)
-
+    buttonLetsStart.addEventListener('click',selectCharacter)    
 }
 
 function selectCharacter(){
@@ -28,9 +37,11 @@ function selectCharacter(){
     } else {
         alert('Selecciona una mascota');
     }
+    
     sectionSelectCharacter.style.display="none"
     sectionWelcomeContainer.style.display="none"
     showMap.style.display = "flex"
+   
     
 }
 
